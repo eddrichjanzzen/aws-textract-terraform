a314fa2be988b552dd11ef960419c104e1560c3d
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-03-27 21:54:20 +0800

    configure lambda to get filename and detect text for uploaded document

--- textract_lambda.py
+++ textract_lambda.py
@@ -1,4 +1,34 @@
 import os
+import boto3
+
 
 def lambda_handler(event, context):
-    return "{} from Lambda!".format(os.environ['greeting'])
+	
+	if event:
+		print("event: ", event)
+
+		file_obj = event['Records'][0]
+		filename = str(file_obj['s3']['object']['key'])
+
+		bucket = os.environ['s3_bucket_name']
+		document = filename
+
+		client = boto3.client('textract')
+
+		#process using S3 object
+		response = client.detect_document_text(
+				Document={
+						'S3Object': {
+								'Bucket': bucket, 
+								'Name': document
+						}
+				}
+		)
+
+		#Get the text blocks
+		blocks = response['Blocks']
+		
+		return {
+				'statusCode': 200,
+				'body': json.dumps(blocks)
+		}           
