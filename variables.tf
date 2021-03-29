5278a30a062b14fe7a23a1faa3fb9224a2733af4
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-03-29 21:53:21 +0800

    update variables

--- variables.tf
+++ variables.tf
@@ -39,39 +39,62 @@ variable "aws_s3_bucket_name" {
   description = "Required - Name of the s3 bucket."    
 }
 
-# DynamoDB Table
-variable "aws_dynamodb_table_name" {
+# DynamoDB Table Document Input
+variable "document_input_table_name" {
   type        = string
   description = "The name of the table, this needs to be unique within a region."
 }
 
-variable "aws_dynamodb_table_hash_key" {
+variable "document_input_table_hash_key" {
   type        = string
   description = "(Required, Forces new resource) The attribute to use as the hash (partition) key. Must also be defined as an attribute, see below."
 }
 
-variable "aws_dynamodb_table_read_capacity" {
+variable "document_input_table_read_capacity" {
   type        = string
   description = "(Optional) The number of read units for this table. If the billing_mode is PROVISIONED, this field is required."
 }
 
-variable "aws_dynamodb_table_write_capacity" {
+variable "document_input_table_write_capacity" {
   type        = string
   description = "(Optional) The number of write units for this table. If the billing_mode is PROVISIONED, this field is required."
 }
 
-# Lambda
-variable "aws_lambda_function_function_name" {
+# DynamoDB Table Document Output
+variable "document_output_table_name" {
   type        = string
-  description = "Required - function_name of the lambda function"    
+  description = "The name of the table, this needs to be unique within a region."
 }
 
-variable "aws_lambda_function_filename" {
+variable "document_output_table_hash_key" {
   type        = string
-  description = "Required - filename of the lambda function ends in filename.zip"    
+  description = "(Required, Forces new resource) The attribute to use as the hash (partition) key. Must also be defined as an attribute, see below."
 }
 
-variable "aws_lambda_function_handler" {
+variable "document_output_table_read_capacity" {
+  type        = string
+  description = "(Optional) The number of read units for this table. If the billing_mode is PROVISIONED, this field is required."
+}
+
+variable "document_output_table_write_capacity" {
+  type        = string
+  description = "(Optional) The number of write units for this table. If the billing_mode is PROVISIONED, this field is required."
+}
+
+
+
+# Text textract lambda 
+variable "docproc_function_name" {
+  type        = string
+  description = "Required - function_name of the docproc function"    
+}
+
+variable "docproc_function_filename" {
+  type        = string
+  description = "Required - filename of docproc function ends in filename.zip"    
+}
+
+variable "docproc_function_handler" {
   type        = string
   description = "Required - Name of the lambda handler"    
 }
@@ -83,12 +106,12 @@ variable "aws_lambda_function_runtime" {
 
 # Lambda SQS Event Source
 
-variable "aws_lambda_event_source_mapping_batch_size" {
+variable "docproc_event_source_mapping_batch_size" {
   type        = number
   description = "Required - Number of messages you want to send the sqs queue at a time. eg. 1 message at a time"    
 }
 
-# Lambda Service Role Name
+# Textract Lambda Service Role
 
 variable "lambda_service_role_name" {
   type        = string
@@ -101,13 +124,37 @@ variable "lambda_service_role_policy_name" {
 }
 
 
-# Lambda Archive file
-variable "archive_file_source_file" {
+# Textract Lambda Archive file
+variable "docproc_archive_file_source_file" {
   type        = string
   description = "Required - source to archive for lambda function eg. lambda_function.py"    
 }
 
-variable "archive_file_output_path" {
+variable "docproc_archive_file_output_path" {
   type        = string
   description = "Required - output path of the archive file for the lambda function eg. lambda_function.zip"    
+}
+
+# Lambda Layer Archive file
+
+variable "lambda_layer_archive_file_source_dir" {
+  type        = string
+  description = "Required - source directory archive for lambda function eg. /src/lambda-layers"    
+}
+
+variable "lambda_layer_archive_file_output_path" {
+  type        = string
+  description = "Required - output path of the archive file for the lambda function eg. lambda_function.zip"    
+}
+
+# Lambda Layer 
+
+variable "lambda_layer_filename" {
+  type        = string
+  description = "Required - filename of the lambda layer"    
+}
+
+variable "lambda_layer_layer_name" {
+  type        = string
+  description = "Required - layer name of the utils lambda layer"    
 }
