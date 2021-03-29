994032bcf50b5d197c352695d353973cc607c8da
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-03-29 21:53:07 +0800

    define outputs

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,21 @@
 # SQS queue
-output "aws_sqs_queue_arn" {
-  value       = aws_sqs_queue.this.arn
+output "sync_sqs_queue_arn" {
+  value       = aws_sqs_queue.sync_queue.arn
   description = "Full ARN of the sqs queue."
 }
 
-output "aws_sqs_queue_name" {
-  value       = aws_sqs_queue.this.name
+output "sync_sqs_queue_name" {
+  value       = aws_sqs_queue.sync_queue.name
   description = "The name of the sqs queue."
 }
 
+output "sync_sqs_queue_id" {
+  value       = aws_sqs_queue.sync_queue.id
+  description = "The name of the id queue."
+}
 
-# S3 bucket
+
+# S3 bucket - Source bucket
 output "aws_s3_bucket_arn" {
   value       = aws_s3_bucket.this.arn
   description = "Full ARN of the sqs queue."
@@ -22,39 +27,75 @@ output "aws_s3_bucket_name" {
 }
 
 
-# DynamoDB Table
-output "aws_dynamodb_table_arn" {
-  value       = aws_dynamodb_table.this.arn
+# DynamoDB Table - Document Input Table
+output "document_input_table_arn" {
+  value       = aws_dynamodb_table.document_input_table.arn
   description = "The arn of the table"
 }
 
-output "aws_dynamodb_table_id" {
-  value       = aws_dynamodb_table.this.id
+output "document_input_table_id" {
+  value       = aws_dynamodb_table.document_input_table.id
   description = "The name of the table"
 }
 
-output "aws_dynamodb_table_stream_arn" {
-  value       = aws_dynamodb_table.this.stream_arn
+output "document_input_table_stream_arn" {
+  value       = aws_dynamodb_table.document_input_table.stream_arn
   description = " The ARN of the Table Stream. Only available when stream_enabled = true"
 }
 
-output "aws_dynamodb_table_stream_label" {
-  value       = aws_dynamodb_table.this.stream_label
+output "document_input_table_stream_label" {
+  value       = aws_dynamodb_table.document_input_table.stream_label
   description = "A timestamp, in ISO 8601 format, for this stream. Note that this timestamp is not a unique identifier for the stream on its own. However, the combination of AWS customer ID, table name and this field is guaranteed to be unique. It can be used for creating CloudWatch Alarms. Only available when stream_enabled = true"
 }
 
-# Lambda function
-output "aws_lambda_function_function_name" {
-  value       = aws_lambda_function.this.function_name
-  description = "The name of the lambda function"
+
+# DynamoDB Table - Document Input Table
+output "document_output_table_arn" {
+  value       = aws_dynamodb_table.document_output_table.arn
+  description = "The arn of the table"
 }
 
-output "aws_lambda_function_handler" {
-  value       = aws_lambda_function.this.handler
-  description = "The handler of the lambda function"
+output "document_output_table_id" {
+  value       = aws_dynamodb_table.document_output_table.id
+  description = "The name of the table"
 }
 
+output "document_output_table_stream_arn" {
+  value       = aws_dynamodb_table.document_output_table.stream_arn
+  description = " The ARN of the Table Stream. Only available when stream_enabled = true"
+}
+
+output "document_output_table_stream_label" {
+  value       = aws_dynamodb_table.document_output_table.stream_label
+  description = "A timestamp, in ISO 8601 format, for this stream. Note that this timestamp is not a unique identifier for the stream on its own. However, the combination of AWS customer ID, table name and this field is guaranteed to be unique. It can be used for creating CloudWatch Alarms. Only available when stream_enabled = true"
+}
+
+
+# Lambda Runtime
 output "aws_lambda_function_runtime" {
-  value       = aws_lambda_function.this.runtime
-  description = "The runtime of the lambda function"
+  value       = aws_lambda_function.docproc.runtime
+  description = "The runtime of the textract lambda function"
+}
+
+# Doc Proc function
+output "docproc_function_name" {
+  value       = aws_lambda_function.docproc.function_name
+  description = "The name of the textract lambda function"
+}
+
+output "docproc_function_handler" {
+  value       = aws_lambda_function.docproc.handler
+  description = "The handler of the textract lambda function"
+}
+
+# Utils Lambda Layer
+
+output "utils_lambda_layer_name" {
+  value       = aws_lambda_layer_version.utils.layer_name
+  description = "The name of the utils lambda layer"
+}
+
+output "utils_lambda_layer_arn" {
+  value        = aws_lambda_layer_version.utils.arn
+  description = "The arn of the utils lambda layer"    
 }
