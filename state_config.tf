5a29608b920364187eba7f64f50b3e7b37dd58ba
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-03-31 16:09:28 +0800

    rename to aws-textract-terraform.tfstate

--- state_config.tf
+++ state_config.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     region         = "ap-southeast-1"
     bucket         = "449276385511-ap-southeast-1-terraform-state"
-    key            = "aws-ecr-terraform.tfstate"
+    key            = "aws-textract-terraform.tfstate"
     encrypt        = "true"
     dynamodb_table = "terraform-state-lock" 
   }
